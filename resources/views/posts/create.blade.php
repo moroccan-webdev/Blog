f985503c7dd4ca326bad82c0de0e08ebd442979b
Web Dev <moroccanwebdeveloper@gmail.com>
2016-12-30 16:11:30 +0000

    Blog with some pretty advanced stuff and an awesome admin panel Adminlte

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,65 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+	{{ trans('adminlte_lang::message.home') }}
+@endsection
+
+@section('stylesheets')
+
+	{!! Html::style('css/parsley.css') !!}
+	{!! Html::style('css/select2.min.css') !!}
+	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+
+	<script>
+		tinymce.init({
+			selector: 'textarea',
+			plugins: 'link code',
+			menubar: false
+		});
+	</script>
+
+@endsection
+
+@section('main-content')
+
+<!-- general form elements disabled -->
+<div class="box box-primary col-md-10 col-md-offset-1">
+
+
+		<h1>Create a new post</h1>
+			<hr>
+				{!! Form::open(['route' => 'posts.store','form data-parsley-validate' => '', 'files' => true])!!}
+				{{ Form::label('title','Title: ')}}
+				{{ Form::text('title', null, array('class' => 'form-control', 'required' => '','maxlength' => '255'))}}
+
+				{{ Form::label('slug','Slug: ')}}
+				{{ Form::text('slug', null, array('class' => 'form-control', 'required' => '', 'minlength' => '5', 'maxlength' => '255'))}}
+
+				{{ Form::label('category_id','Category: ')}}
+					<select class="form-control" name="category_id">
+						@foreach($categories as $category)
+							<option value="{{ $category->id }}">{{ $category->name }}</option>
+						@endforeach
+					</select>
+
+				{{ Form::label('tags', 'Tags:') }}
+					<select class="form-control select2-multi" name="tags[]" multiple="multiple">
+						@foreach($tags as $tag)
+							<option value='{{ $tag->id }}'>{{ $tag->name }}</option>
+						@endforeach
+
+					</select>
+
+				{{ Form::label('featured_img', 'Upload a Featured Image') }}
+					{{ Form::file('featured_img') }}
+
+				{{ Form::label('body','Post body: ')}}
+				{{ Form::textarea('body', null, array('class' => 'form-control'))}}
+
+				{{ Form::submit('Create Post',  array('class' => 'btn btn-success btn-lg btn-block', 'style'=> 'margin-top: 20px'))}}
+						{!! Form::close() !!}
+
+
+</div>
+
+@endsection
---
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        <a href="https://google.com"></a><b>Version: 2.1</b></a>. 
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="https://www.linkedin.com/in/AbdennourMerghad">Merghad Abdennour</a>.</strong>
+</footer>
